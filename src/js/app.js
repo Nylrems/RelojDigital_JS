36e49c02fc671381826b77e39e64af1ebeed87cb
Slermyn <smerlyn_alejandro@outlook.com>
2022-03-04 21:31:27 -0400

    reloj funcional terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,50 @@
 const mostrarReloj = () => {
   let fecha = new Date();
-  let hora = fecha.getHours();
-  let minutos = fecha.getMinutes();
-  let segundos = fecha.getSeconds();
-  document.getElementById('hora').innerHTML = `${hora}: ${minutos}: ${segundos}`
-  console.log(`${hora}:${minutos}:${segundos}`); 
+  let hora = formatoHora(fecha.getHours());
+  let minutos = formatoHora(fecha.getMinutes());
+  let segundos = formatoHora(fecha.getSeconds());
+
+  let anio = fecha.getFullYear();
+
+  const meses = [
+    "Ene",
+    "Feb",
+    "Mar",
+    "Abr",
+    "May",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dic",
+  ];
+  const dias = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+
+  let diaSemana = dias[fecha.getDay()];
+  let dia = fecha.getDay();
+
+  let mes = meses[fecha.getMonth()];
+
+  console.log(`${hora}:${minutos}:${segundos}`);
+  console.log(`${diaSemana}, ${dia} / ${mes} / ${anio}`);
+
+  document.getElementById(
+    "hora"
+  ).innerHTML = `${hora}: ${minutos}: ${segundos}`;
+  document.getElementById(
+    "fecha"
+  ).innerHTML = `${diaSemana}, ${dia} / ${mes} / ${anio}`;
+
+  document.getElementById('contenedor').classList.toggle('animar')
 };
-setInterval(mostrarReloj, 1000)
 
+const formatoHora = (hora) => {
+  if (hora < 10) {
+    hora = "0" + hora;
+  }
+  return hora;
+};
 
-
+setInterval(mostrarReloj, 1000);
